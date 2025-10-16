c2303d3d4dfc6f449f24908ec39eac6f79d63c31
rutamk <rutam2003@gmail.com>
2025-10-16 23:38:43 +0530

    registeration stuff

---
+++ src/main/java/com/eazybytes/eazystore/security/EazyStoreUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,42 @@
+package com.eazybytes.eazystore.security;
+
+import com.eazybytes.eazystore.entity.Customer;
+import com.eazybytes.eazystore.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class EazyStoreUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: " + username)
+        );
+        if(passwordEncoder.matches(pwd, customer.getPasswordHash())){
+            return new UsernamePasswordAuthenticationToken(customer,null, Collections.emptyList());
+        } else {
+            throw new BadCredentialsException("Invalid password");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
