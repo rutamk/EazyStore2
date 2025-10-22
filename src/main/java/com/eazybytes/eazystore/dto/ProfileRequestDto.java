4eb4a48bc79c3598499ef82b7a611ea25af19671
rutamk <rutam2003@gmail.com>
2025-10-22 15:38:27 +0530

    payments otw

--- src/main/java/com/eazybytes/eazystore/dto/ProfileRequestDto.java
+++ src/main/java/com/eazybytes/eazystore/dto/ProfileRequestDto.java
@@ -39,7 +39,7 @@ public class ProfileRequestDto {
     private String postalCode;
 
     @NotBlank(message = "Country is required")
-    @Size(min = 3, max = 30, message = "The length of the country should be between 3 and 30 characters")
+    @Size(min = 2, max = 2, message = "The length of the country should be exactly 2 characters")
     private String country;
 
 }
